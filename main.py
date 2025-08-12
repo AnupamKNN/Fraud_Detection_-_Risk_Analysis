b18fa66dd23c07204c336add6e82e5b1d299e768
Anupam Singh <anupam848484@gmail.com>
2025-08-12 13:42:41 +0530

    Implement model trainer with MLflow experiment tracking, model registry, and DVC integration

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from src.fraud_detection.components.data_ingestion import DataIngestion
 from src.fraud_detection.components.data_validation import DataValidation
 from src.fraud_detection.components.data_transformation import DataTransformation
+from src.fraud_detection.components.model_trainer import ModelTrainer
 
 
 from src.fraud_detection.exception.exception import FraudDetectionException
 from src.fraud_detection.logging.logger import logging
-from src.fraud_detection.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from src.fraud_detection.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 
 import sys
 if __name__ == "__main__":
@@ -15,7 +16,7 @@ if __name__ == "__main__":
         # Data Ingestion
         data_ingestion_config = DataIngestionConfig(training_pipeline_config = training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config= data_ingestion_config)
-        logging.info("Initiating data ingestion")
+        logging.info("Initiating Data Ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion completed")
 
@@ -23,7 +24,7 @@ if __name__ == "__main__":
         data_validation_config = DataValidationConfig(training_pipeline_config= training_pipeline_config)
         data_validation = DataValidation(data_ingestion_artifact= data_ingestion_artifact,
                                          data_validation_config= data_validation_config)
-        logging.info("Initiating data validation")
+        logging.info("Initiating Data Validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation completed")
 
@@ -31,9 +32,17 @@ if __name__ == "__main__":
         data_transformation_config = DataTransformationConfig(training_pipeline_config= training_pipeline_config)
         data_transformation = DataTransformation(data_validation_artifact= data_validation_artifact,
                                                  data_transformation_config= data_transformation_config)
-        logging.info("Initiating data transformation")
+        logging.info("Initiating Data Transformation")
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Data Transformation completed")
+
+        # Model Trainer
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config= training_pipeline_config)
+        model_trainer = ModelTrainer(data_transformation_artifact= data_transformation_artifact,
+                                     model_trainer_config= model_trainer_config)
+        logging.info("Initiating Model Training")
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training completed")
     
     except Exception as e:
         raise FraudDetectionException(e, sys)
---
+++ src/fraud_detection/utils/ml_utils/metric/classification_metric.py
@@ -0,0 +1,19 @@
+from src.fraud_detection.entity.artifact_entity import ClassificationMetricArtifact
+from src.fraud_detection.exception.exception import FraudDetectionException
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def get_classification_score(y_true,y_pred):
+    try:
+        model_accuracy_score = accuracy_score(y_true, y_pred)
+        model_precision_score = precision_score(y_true, y_pred)
+        model_recall_score = recall_score(y_true, y_pred)
+        model_f1_score = f1_score(y_true, y_pred)
+        classification_metric = ClassificationMetricArtifact(
+            accuracy_score=model_accuracy_score,
+            precision_score=model_precision_score,
+            recall_score=model_recall_score,
+            f1_score=model_f1_score
+        )
+        return classification_metric
+    except Exception as e:
+        raise FraudDetectionException(e, sys)
